92d73b794e436e7d3ee10bc2d4cd1311d9f68cf3
Aneep Tandel <aneepct@live.com>
2019-04-06 07:55:46 +0530

    Update redux store

--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,20 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+// import {IRegisterStates}  from "../src/interface/auth/registerState";
 
-const initialState = {};
+const initialState = {
+
+};
+
 const middleware = [thunk];
-
 const store = createStore(
-  rootReducer, 
+  rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  ) 
+  composeWithDevTools(
+    applyMiddleware(...middleware)
+  )
 );
 
 export default store;
